2256e5954682d85fff6913dd9838d8cf90f2ac80
Anthony <100038382+KeynoIX@users.noreply.github.com>
2024-10-08 17:36:38 +0200

    Update temp_proco.py

--- temp_proco.py
+++ temp_proco.py
@@ -1,16 +1,11 @@
-import re, subprocess
- 
-def check_CPU_temp():
-    temp = None
-    err, msg = subprocess.getstatusoutput('vcgencmd measure_temp')
-    if not err:
-        m = re.search(r'-?\d\.?\d*', msg)   # a solution with a  regex
-        try:
-            temp = float(m.group())
-        except ValueError: # catch only error needed
-            pass
-    return temp, msg
- 
-temp, msg = check_CPU_temp()
-print(f"temperature {temp}°C")
-print(f"full message {msg}")
+import wmi #normal c'est pour Windows 
+w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+temperature_infos = w.Sensor()
+for sensor in temperature_infos:
+    if sensor.SensorType==u'Temperature':
+        print(sensor.Name)
+        print(sensor.Value)
+
+# ------------------------------------------------------------------
+#version pour Fedora
+
